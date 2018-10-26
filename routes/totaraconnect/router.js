45278aa0ad3a9aea9ca3e18baa93175e8b0a407c
Tom Taylor <hello@tomtaylor.name>
2018-10-26 22:43:26 +0100

    Add catch-all route
    #gottacatchemall

--- routes/totaraconnect/router.js
+++ routes/totaraconnect/router.js
@@ -1,27 +1,34 @@
 var express = require('express');
+var configuration = require('../../lib/configuration');
 var controller = require('./controller');
 var errors = require('./errors');
 
 var router = express.Router();
 var crud = controller.crudWrapper(router);
 
+crud.get('/', (req, res, next) => {
+  const URL = configuration.getConfig('rootUrl') + req.originalUrl;
+  res.json({
+    generate_token_url: `${URL}/generatetoken`,
+    publish_course_url: `${URL}/publish/{id}`,
+    tokens_url: `${URL}/tokens`,
+    token_url: `${URL}/token/{id}`,
+    test_connection_url: `${URL}/testconnection`,
+    courses_url: `${URL}/courses`,
+    scorm_url: `${URL}/scorm/{id}`
+  });
+});
+// UI endpoints
 crud.get('/generatetoken', controller.generateToken);
-
-// the below should only come from the UI
 crud.get('/publish/:id', controller.publishCourse);
 crud.get('/tokens', controller.getTokens);
 crud.delete('/token/:id', controller.deleteToken);
-
-// Token authentication middleware
-router.use(controller.authenticate);
-
-crud.get('/testconnection', controller.testConnection);
-
-// Stores the request's user details for use later
-router.use(controller.memoiseUser);
-
-crud.get('/courses', controller.getCourses);
-crud.get('/scorm/:id', controller.getScorm);
+// API endpoints
+crud.get('/testconnection', controller.authenticate, controller.testConnection);
+crud.get('/courses', controller.authenticate, controller.memoiseUser, controller.getCourses);
+crud.get('/scorm/:id', controller.authenticate, controller.memoiseUser, controller.getScorm);
+// catch-all
+crud.unmatched(controller.handleUnmatched);
 
 // Error handling
 router.use(errors.handler);
